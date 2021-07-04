ec6d646e71076914ab872c8d33e426b3b8a1149a
leonardototti <leonardo.totti@edu.unifil.br>
2021-07-04 17:28:46 -0400

    finished auth wall screens

--- src/app/preferencias/preferencias.page.ts
+++ src/app/preferencias/preferencias.page.ts
@@ -12,4 +12,32 @@ export class PreferenciasPage implements OnInit {
   ngOnInit() {
   }
 
+  topics:any = [
+    {name:'Jogos', active:false, size: 6},
+    {name:'Animes', active:false, size: 6},
+    {name:'Filmes', active:false, size: 4},
+    {name:'Séries', active:false, size: 4},
+    {name:'Músicas', active:false, size: 4},
+    {name:'Programação', active:false, size: 6},
+    {name:'Quadrinhos', active:false, size: 6}
+  ];
+
+  selectedTopics:any = 0;
+  aux:any = 0;
+
+  selectTopic(topic) {
+    topic.active = !topic.active;
+
+    this.getSelectedTopics();
+  }
+
+  getSelectedTopics() {
+    this.aux = 0;
+    this.topics.map(topic =>{
+      topic.active ? this.aux++ : null;
+    })
+
+    this.selectedTopics = this.aux;
+  }
+
 }
